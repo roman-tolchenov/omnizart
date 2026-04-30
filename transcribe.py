2a68407128ce4a1604c940a01e73c779304d6d94
Roman Tolchenov <roman.tolchenov@gmail.com>
2026-04-30 16:21:37 +0000

    transcribe uses default model

--- transcribe.py
+++ transcribe.py
@@ -11,26 +11,24 @@ this_dir = Path(__file__).parent
 
 def transcribe_cmd(
     input_path: Path,
-    model_path: Path,
     output_folder: Path,
 ):
     silence_tensorflow()
-    music.app.transcribe(input_path, model_path, output=output_folder)
+    music.app.transcribe(input_path, None, output=output_folder)
 
 
 def main():
     input_path = Path(sys.argv[1])
-    model_path = Path(sys.argv[2])
-    output_folder = Path(sys.argv[3])
+    output_folder = Path(sys.argv[2])
     output_folder.mkdir(parents=True, exist_ok=True)
 
     if input_path.is_file():
-        transcribe_cmd(input_path, model_path, output_folder)
+        transcribe_cmd(input_path, output_folder)
     else:
         for input_path in input_path.glob('**/*.wav'):
             rel_path = input_path.parent.relative_to(input_path)
             out_folder = output_folder / rel_path
-            transcribe_cmd(input_path, model_path, out_folder)
+            transcribe_cmd(input_path, out_folder)
 
 
 if __name__ == "__main__":
